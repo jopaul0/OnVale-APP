07c317736adaf9f56a89c8630962e1760bbac078
João Paulo <jopaulo.as8@gmail.com>
2025-07-18 13:17:14 -0300

    feat: Adicionando link para o whatsapp.

--- OnValeApp/components/KeyboardScroll.tsx
+++ OnValeApp/components/KeyboardScroll.tsx
@@ -1,33 +1,39 @@
-import React, {
-    FC,
-    ReactNode
-} from 'react';
+import React, { FC, ReactNode } from 'react';
 import {
     KeyboardAvoidingView,
     ScrollView,
     Platform,
     StyleSheet,
     Keyboard,
-    TouchableWithoutFeedback
+    TouchableWithoutFeedback,
+    View,
 } from 'react-native';
 
 const KeyboardScroll: FC<{ children: ReactNode }> = ({ children }) => (
     <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : undefined} style={styles.wrapper}
-        keyboardVerticalOffset={60}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 20 : 0}
+        style={styles.wrapper}
     >
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
             <ScrollView
-                contentContainerStyle={styles.wrapper}
+                contentContainerStyle={styles.scrollContent}
                 keyboardShouldPersistTaps="handled"
             >
-                {children}
+                <View style={styles.fillSpace}>{children}</View>
             </ScrollView>
         </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
 );
+
 export default KeyboardScroll;
 
 const styles = StyleSheet.create({
-    wrapper: { flex: 1 }
+    wrapper: { flex: 1 },
+    scrollContent: {
+        flexGrow: 1,
+    },
+    fillSpace: {
+        flex: 1, // garante que o conteúdo preencha o restante da tela
+    },
 });
--- OnValeApp/screens/LoginScreen.tsx
+++ OnValeApp/screens/LoginScreen.tsx
@@ -1,27 +1,31 @@
-//REACT
 import React, { useState } from 'react';
 import {
   View,
   Text,
   StyleSheet,
   Image,
+  Linking
 } from 'react-native';
 
-//COMPONENTS
 import SafeArea from '../components/SafeArea';
 import KeyboardScroll from '../components/KeyboardScroll';
 import SimpleButton from '../components/SimpleButton';
 import SimpleTextInput from '../components/SimpleTextInput';
 
 export default function LoginScreen() {
-  //STATES
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
 
-  //HANDLER
   function handleLogin() {
     console.log('Login:', email, senha);
   }
+
+  function handleSaibaMais() {
+    Linking.openURL(
+      'https://api.whatsapp.com/send/?phone=5512982044681&text=Ol%C3%A1%20OnVale!%20Gostaria%20de%20saber%20como%20fa%C3%A7o%20para%20ter%20um%20cadastro%20no%20OnVale%20APP.&type=phone_number&app_absent=0'
+    );
+  }
+
 
   return (
     <SafeArea>
@@ -49,8 +53,12 @@ export default function LoginScreen() {
             onChange={setSenha}
           />
           <SimpleButton title="Login" onPress={handleLogin} />
+
           <Text style={styles.footerText}>
-            Não possui cadastro? <Text style={styles.link}>Saiba mais</Text>
+            Não possui cadastro?{' '}
+            <Text style={styles.link} onPress={handleSaibaMais}>
+              Saiba mais
+            </Text>
           </Text>
         </View>
       </KeyboardScroll>
@@ -58,7 +66,6 @@ export default function LoginScreen() {
   );
 }
 
-//STYLES
 const styles = StyleSheet.create({
   LogoContainer: {
     alignItems: 'center',
